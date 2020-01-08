9e566af2ef01844ff547ab812a44cf7a1dfdf4f0
Gabor Jelenfi <dzsub4@gmail.com>
2020-01-08 15:20:51 +0100

    added AppRouterModule + path

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,11 +9,12 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService) { }
+
   getHeores(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
 }
