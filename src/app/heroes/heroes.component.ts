bbd3414fc18e1d8dddcc3ac88fa1b5d5e3f59ae3
Gabor Jelenfi <dzsub4@gmail.com>
2020-01-08 15:39:18 +0100

    + dashboard view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,7 +23,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeores()
+    this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
 
